5ff34c02fb0e821beb4903ae8658d6b67ffaef5b
Vivek Joshi <vivekjoshi76772@gmail.com>
2021-07-14 09:46:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+
+
+function App() {
+  return (
+
+    // BEM convention
+
+    <div className="app">
+
+      {/* Banner */}
+      
+
+
+      {/* Header */}
+      <Header />
+      {/* Home */}
+
+    </div>
+  );
+}
+
+export default App;
